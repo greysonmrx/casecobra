4348809d5f6e024deffc473078990ebaa51fb21e
Greyson Filho <greysonmrx@gmail.com>
2024-05-20 10:41:13 -0300

    feat: create Reviews component

---
+++ src/utils/splitArray.ts
@@ -0,0 +1,17 @@
+function splitArray<T>(array: Array<T>, numParts: number) {
+  const result: Array<Array<T>> = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const index = i % numParts;
+
+    if (!result[index]) {
+      result[index] = [];
+    }
+
+    result[index].push(array[i]);
+  }
+
+  return result;
+}
+
+export { splitArray };
